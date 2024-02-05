419cf24b05d5137d7c8f59ccaf06514572bbe3a6
alvinampoloquio24 <alvinampoloquio24.@gmail.com>
2024-02-05 09:25:35 +0800

    change model

--- src/models/user.js
+++ src/models/user.js
@@ -3,9 +3,23 @@ const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
 const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+  },
+  role: {
+    type: String,
+    default: "user",
+  },
   email: {
     type: String,
     required: true,
+  },
+  gender: {
+    type: String,
   },
   password: {
     type: String,
