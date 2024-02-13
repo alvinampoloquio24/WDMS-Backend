d4d1e3e848b62ab019ca61d7fa2e9a8163549299
Alvin Ampoloquio <158123377+alvinampoloquio24@users.noreply.github.com>
2024-02-13 10:56:54 +0800

    samoka

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -63,7 +63,7 @@ const login = async (req, res) => {
 
 const changePassword = async (req, res) => {
   try {
-    const id = req.user._id;
+    const id = req.params.id;
     if (!id) {
       return res.status(400).json({ message: "Id is required. " });
     }
