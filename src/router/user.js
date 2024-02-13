ebcde42ef32f0f58dae29dd3aaddbe4f88e9849e
Alvin Ampoloquio <158123377+alvinampoloquio24@users.noreply.github.com>
2024-02-13 11:41:22 +0800

    Update user.js

--- src/router/user.js
+++ src/router/user.js
@@ -15,6 +15,6 @@ router.get("/fetchAllUser", getAllUser);
 router.delete("/deleteUser/:id", deleteUser);
 router.patch("/updateUser/:id", updateUser);
 router.post("/login", login);
-router.post("/changePassword", changePassword); // corrected typo here
+router.post("/changePassword",auth, changePassword); // corrected typo here
 
 module.exports = router;
